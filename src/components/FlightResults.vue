0557c1f7ef0d99f3695283f5c7df4ae2f373450c
Sciederrick <derrickmbarani@gmail.com>
2020-11-13 12:15:19 +0300

    Booking Enquiry Results(User Interface): 95% done

--- src/components/FlightResults.vue
+++ src/components/FlightResults.vue
@@ -3,12 +3,29 @@
     <!-- results nav -->
     <ResultsNav/>
     <!-- header -->
-    <div class="">
-
+    <div class="mx-1 md:mx-2 lg:mx-20 lg:my-20 flex flex-row justify-between md:justify-around items-center p-2">
+      <div class="w-40 h-32 md:w-48 md:h-40 lg:w-64 lg:h-56 bg-gray-700 flex flex-col justify-end rounded-sm">
+        <div class="bg-red-600">
+          <p class="text-white text-sm lg:text-lg p-1">Nairobi</p>
+        </div>
+      </div>
+      <div class="w-24 h-32 md:w-32 md:h-32 bg-white flex justify-center items-center">
+        <fa-icon class="hidden md:block text-gray-400" :icon="['fas','plane']" size="3x"/>
+        <fa-icon class="md:hidden text-gray-400" :icon="['fas','plane']" size="2x"/>
+      </div>
+      <div class="w-40 h-32 md:w-48 md:h-40 lg:w-64 lg:h-56 bg-gray-700 flex flex-col justify-end rounded-sm relative">
+        <span class="absolute top-0 right-0 m-2 text-xs md:text-sm bg-white text-gray-800 rounded p-1 font-semibold">70,000 KES</span>
+        <div class="bg-red-600">
+          <p class="text-white text-sm lg:text-lg p-1">New York</p>
+        </div>
+      </div>
     </div>
     <!-- results -->
-    <div class="flex flex-wrap justify-center">
-      <table class="table-fixed my-4 lg:m-6 mx-auto text-xs lg:text-base shadow-lg">
+    <div class="flex flex-wrap justify-center md:justify-center lg:pt-8">
+      <table class="w-full md:w-auto table-fixed my-4 lg:m-6 mx-auto text-xs lg:text-base shadow-lg lg:shadow-xl">
+        <tr>
+          <td colspan="3"><hr/></td>
+        </tr>
         <tr class="m-1">
           <td class="border-4 border-white w-1/3 p-2 bg-gray-100 font-semibold">0500 HRS</td>
           <td class="w-1/3 py-2"><fa-icon class="text-gray-200" :icon="['far', 'clock']" size="2x"/></td>
@@ -23,7 +40,10 @@
           <td class="border-4 border-white p-2 bg-gray-100" colspan="3">ET 319 Boeing 777-300ER</td>
         </tr>
       </table>
-      <table class="table-fixed my-4 lg:m-6 mx-auto text-xs lg:text-base shadow-lg">
+      <table class="w-full md:w-auto table-fixed my-4 lg:m-6 mx-auto text-xs lg:text-base shadow-lg lg:shadow-xl">
+        <tr>
+          <td colspan="3"><hr/></td>
+        </tr>
         <tr class="m-1">
           <td class="border-4 border-white w-1/3 p-2 bg-gray-100 font-semibold">0500 HRS</td>
           <td class="w-1/3 py-2"><fa-icon class="text-gray-200" :icon="['far', 'clock']" size="2x"/></td>
@@ -38,7 +58,10 @@
           <td class="border-4 border-white p-2 bg-gray-100" colspan="3">ET 319 Boeing 777-300ER</td>
         </tr>
       </table>
-      <table class="table-fixed my-4 lg:m-6 mx-auto text-xs lg:text-base shadow-lg">
+      <table class="w-full md:w-auto table-fixed my-4 lg:m-6 mx-auto text-xs lg:text-base shadow-lg lg:shadow-xl">
+        <tr>
+          <td colspan="3"><hr/></td>
+        </tr>
         <tr class="m-1">
           <td class="border-4 border-white w-1/3 p-2 bg-gray-100 font-semibold">0500 HRS</td>
           <td class="w-1/3 py-2"><fa-icon class="text-gray-200" :icon="['far', 'clock']" size="2x"/></td>
@@ -53,7 +76,10 @@
           <td class="border-4 border-white p-2 bg-gray-100" colspan="3">ET 319 Boeing 777-300ER</td>
         </tr>
       </table>
-      <table class="table-fixed my-4 lg:m-6 mx-auto text-xs lg:text-base shadow-lg">
+      <table class="w-full md:w-auto table-fixed my-4 lg:m-6 mx-auto text-xs lg:text-base shadow-lg lg:shadow-xl">
+        <tr>
+          <td colspan="3"><hr/></td>
+        </tr>
         <tr class="m-1">
           <td class="border-4 border-white w-1/3 p-2 bg-gray-100 font-semibold">0500 HRS</td>
           <td class="w-1/3 py-2"><fa-icon class="text-gray-200" :icon="['far', 'clock']" size="2x"/></td>
@@ -73,6 +99,7 @@
 </template>
 
 <script>
+import ResultsNav from '@/components/ResultsNav.vue'
 export default {
 name:'FlightResults',
 components:{
--- src/components/resultsNav.vue
+++ src/components/resultsNav.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="resultsNav" class="flex bg-gray-900">
+  <div id="resultsNav" class="flex justify-between py-2 px-3 md:px-5 lg:px-16 md:py-3 lg:py-4 my-4 md:my-5 lg:mx-2 bg-gray-800 text-sm md:text-base lg:text-lg text-white">
     <span>4 results</span>
-    <span>Go back</span>
+    <span class="underline">Go back</span>
   </div>
 </template>
 
